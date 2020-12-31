163c4d5366927232b8940bc2ab61938d8b4b9723
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-31 14:18:24 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Transcripts
+
+Simple Python script to get transcripts from YouTube videos.
+
+### Run
+
+- `conda activate transcripts`
---
+++ script.py
@@ -0,0 +1,12 @@
+###--- IMPORTS ---###
+import settings
+
+
+###--- FUNCTIONS ---###
+def get_transcripts():
+    pass
+
+
+###--- DRIVER CODE ---###
+if __name__ == "__main__":
+    pass
