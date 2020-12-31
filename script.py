35c09d47cfe4dd5a18485c25cff0d5713db17824
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-31 14:55:21 -0600

    grabbing transcript and creating a human-readable paragraph

--- script.py
+++ script.py
@@ -1,19 +1,32 @@
 ###--- IMPORTS ---###
 import settings
-import requests
 from youtube_transcript_api import YouTubeTranscriptApi
 # https://pypi.org/project/youtube-transcript-api/
+
 
 ###--- GLOBAL VARIABLES ---###
 # https://www.youtube.com/watch?v=F2KcZGwntgg
 youtube_video_id = 'F2KcZGwntgg'
 
+
 ###--- FUNCTIONS ---###
 
-
 def get_transcripts():
+    '''
+    Gets transcript object: list of dictionaries. Example:
+    "{'text': "you may not realize it but we're", 'start': 1.25, 'duration': 4.96}, ..."
+    Then loops thru list to create one, human-readable string.
+    '''
     global youtube_video_id
-    print(YouTubeTranscriptApi.get_transcript(youtube_video_id))
+
+    entire_transcript = YouTubeTranscriptApi.get_transcript(youtube_video_id)
+
+    clean_transcript = ''
+
+    for transcript in entire_transcript:
+        clean_transcript += transcript['text'] + '\n'
+
+    print(clean_transcript)
 
 
 ###--- DRIVER CODE ---###
