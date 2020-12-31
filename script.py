e2c80591201c42fefd993a352998882ac58f9ea3
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-31 14:49:26 -0600

    external library, successful response

--- script.py
+++ script.py
@@ -1,12 +1,21 @@
 ###--- IMPORTS ---###
 import settings
+import requests
+from youtube_transcript_api import YouTubeTranscriptApi
+# https://pypi.org/project/youtube-transcript-api/
 
+###--- GLOBAL VARIABLES ---###
+# https://www.youtube.com/watch?v=F2KcZGwntgg
+youtube_video_id = 'F2KcZGwntgg'
 
 ###--- FUNCTIONS ---###
+
+
 def get_transcripts():
-    pass
+    global youtube_video_id
+    print(YouTubeTranscriptApi.get_transcript(youtube_video_id))
 
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
-    pass
+    get_transcripts()
