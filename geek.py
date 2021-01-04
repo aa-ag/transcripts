ed09b2e791cdcc1eea44b3aacf701c2c1b4a5001
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-04 17:42:46 -0600

    sorted as dictionary from most repetitive to least repeated word

--- geek.py
+++ geek.py
@@ -13,8 +13,11 @@ def count_words():
 
         # Selection that meets a conditional
         repetitive_words = {k: v for k, v in word_count.items() if v > 10}
-        print(repetitive_words)
+        # print(repetitive_words)
         # {'the': 62, 'of': 36, 'in': 19, 'and': 20, 'a': 47, 'is': 26, 'silicon': 16, 'to': 34, 'with': 12}
+
+        print(dict(sorted(repetitive_words.items(),
+                          key=lambda item: item[1], reverse=True)))
 
 
 ###--- DRIVER CODE ---###
